870f02e03e2954a39b7805266e96e0e3779dec1e
yan.liu <yan.liu@sumscope.com>
2016-11-16 20:09:02 +0800

    hello

---
+++ CodeLibrary/CodeLibraryDB/DB.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace CodeLibraryDB
+{
+    public class DB
+    {
+        private static readonly string connString = @"Data Source=E:\VisualStudioCode\CodeLibrary\CodeLibrary\CodeLibrary\App_Data\CodeLibrary.db;";
+
+        public void Open()
+        {
+
+            SQLiteConnection conn = new SQLiteConnection(connString);
+            conn.Open();
+
+            string sql = "select * from test";
+
+            var s=conn.Query(sql).ToList();
+
+            conn.Close();
+        }
+    }
+}
